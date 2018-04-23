a18b5dc3ca25390d8b88f6f6249062f1171c28bf
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2018-04-23 12:24:32 -0400

    Added dev mode previews

--- cortex-preview.php
+++ cortex-preview.php
@@ -47,6 +47,7 @@ add_action('cortex/save_block', function($document, $id) {
 add_action('admin_enqueue_scripts', function() {
 
 	wp_enqueue_script('cortex-preview-main', plugins_url('scripts/main.js', __FILE__));
+	wp_enqueue_style('cortex-preview-styles', plugins_url('styles/styles.css', __FILE__));
 
 	wp_localize_script('cortex-preview-main', 'CortexPreviewSettings', array(
 		'serverUrl' => get_option('cortex_preview_server_url'),
@@ -126,6 +127,11 @@ add_action('wp_ajax_cortex_preview_update', function() {
 add_filter('get_twig', function($twig) {
 
 	$generate_preview = function($post) {
+
+		if (cortex_preview_is_local_dev()) {
+			cortex_preview_render_simple_preview($post);
+			return;
+		}
 
 		$id = $post->ID;
 		$document = $post->post_parent;
@@ -581,6 +587,287 @@ function _cortex_render_images($id, $url, $ver) {
 	<?php
 }
 
+
+/**
+ * Tries to detect whether this
+ * @function cortex_preview_options_page
+ * @since 0.1.0
+ */
+function cortex_preview_is_local_dev() {
+
+	$server_name = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : null;
+	$server_addr = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : null;
+	$remote_addr = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+
+	if ($server_addr && $remote_addr &&
+		$server_addr == $remote_addr) {
+		return true;
+	}
+
+	if ($server_name == 'localhost' ||
+		substr($server_name, strrpos($server_name, '.') + 1) == 'test' ||
+		substr($server_name, 0, 3) == '10.' ||
+        substr($server_name, 0, 7) == '192.168') {
+		return true;
+	}
+
+	$http_host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null;
+
+	if ($http_host == 'localhost' ||
+		substr($http_host, strrpos($http_host, '.') + 1) == 'test' ||
+		substr($http_host, 0, 3) == '10.' ||
+        substr($http_host, 0, 7) == '192.168') {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Renders a text prewview.
+ * @function cortex_preview_render_simple_preview
+ * @since 0.1.0
+ */
+function cortex_preview_render_simple_preview($post) {
+
+	$objects = get_field_objects($post->ID);
+
+	if ($objects == false) {
+		echo '<div class="cortex-preview">';
+		echo '<i>' . __('This block has not been saved yet.', 'cortex-preview') . '</i>';
+		echo '</div>';
+		return;
+	}
+
+	echo '<table class="cortex-preview-simple-table">';
+
+	foreach ($objects as $field) {
+
+		echo '<tr>';
+
+		$label = $field['label'];
+		$value = $field['value'];
+
+		echo '<td class="cortex-preview-simple-label"><strong>' . $label . '</strong></td>';
+		echo '<td class="cortex-preview-simple-value">';
+
+		cortex_preview_render_simple_field_preview($field);
+
+		echo '</td>';
+		echo '</tr>';
+	}
+
+	echo '</table>';
+}
+
+/**
+ * Renders a field prewview.
+ * @function cortex_preview_render_text_preview
+ * @since 0.1.0
+ */
+function cortex_preview_render_simple_field_preview($field) {
+
+	$preview = apply_filters('cortex_preview_render_' . $field['type'], $field);
+
+	if ($preview === null || is_string($preview) == false) {
+		$preview = $field['value'];
+	}
+
+	_cortex_print($preview, $field);
+}
+
+/**
+ * Previews flexible_content field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_flexible_content', function($field) {
+	return _cortex_buff(function() use($field) {
+		foreach (_cortex_process_flexible_content($field) as $sub_field) {
+			echo '<div class="cortex-preview-row-label">' . $sub_field['label'] . '</div>';
+			echo '<div class="cortex-preview-row-value">';
+			cortex_preview_render_simple_field_preview($sub_field);
+			echo '</div>';
+		}
+	});
+});
+
+/**
+ * Previews flexible_content field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_repeater', function($field) {
+	return _cortex_buff(function() use($field) {
+		foreach (_cortex_process_repeater($field) as $sub_field) {
+			echo '<div class="cortex-preview-row-label">' . $sub_field['label'] . '</div>';
+			echo '<div class="cortex-preview-row-value">';
+			cortex_preview_render_simple_field_preview($sub_field);
+			echo '</div>';
+		}
+	});
+});
+
+/**
+ * Previews text field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_text', function($field) {
+	return $field['value'] ? $field['value'] : $field['default_value'];
+});
+
+/**
+ * Previews wysiwyg field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_wysiwyg', function($field) {
+	return $field['value'] ? $field['value'] : $field['default_value'];
+});
+
+/**
+ * Previews image field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_image', function($field) {
+	return $field['value'] ? sprintf('<img src="%s">', $field['value']['url']) : null;
+});
+
+/**
+ * Previews radio field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_radio', function($field) {
+	return isset($field['choices'][$field['value']]) ? $field['choices'][$field['value']] : null;
+});
+
+/**
+ * Previews checkbox field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_checkbox', function($field) {
+	return isset($field['choices'][$field['value']]) ? $field['choices'][$field['value']] : null;
+});
+
+/**
+ * Previews select field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_select', function($field) {
+	return isset($field['choices'][$field['value']]) ? $field['choices'][$field['value']] : null;
+});
+
+/**
+ * Previews relationship field.
+ * @since 0.1.0
+ */
+add_filter('cortex_preview_render_relationship', function($field) {
+	return _cortex_buff(function() use($field) {
+		foreach ($field['value'] as $value) {
+			echo '<div class="cortex-preview-row-value">';
+			echo $value->post_title;
+			echo '</div>';
+		}
+	});
+});
+
+/**
+ * @function _cortex_process_flexible_content
+ * @since 0.1.0
+ * @hidden
+ */
+function _cortex_process_flexible_content($field) {
+
+	$fields = array();
+	$layouts = array();
+
+	foreach ($field['layouts'] as $layout) {
+		$layouts[$layout['name']] = $layout;
+	}
+
+	foreach ($field['value'] as $value) {
+
+		$layout = $layouts[$value['acf_fc_layout']];
+
+		if ($layout == null) {
+			continue;
+		}
+
+		foreach ($value as $key => $val) {
+
+			if ($key == 'acf_fc_layout') {
+				continue;
+			}
+
+			foreach ($layout['sub_fields'] as $sub_field) {
+				if ($sub_field['name'] == $key) {
+					$data = $sub_field;
+					$data['value'] = $val;
+					$fields[] = $data;
+				}
+			}
+		}
+	}
+
+	return $fields;
+}
+
+/**
+ * @function _cortex_process_repeater
+ * @since 0.1.0
+ * @hidden
+ */
+function _cortex_process_repeater($field) {
+
+	$fields = array();
+
+	foreach ($field['sub_fields'] as $i => $sub_field) {
+		$field = $sub_field;
+		$field['value'] = $field['value'][$i];
+		$fields[] = $field;
+	}
+
+	return $fields;
+}
+
+/**
+ * @function _cortex_print
+ * @since 0.1.0
+ * @hidden
+ */
+function _cortex_print($value, $field) {
+
+	if ($value == null) {
+		echo __('Empty', 'cortex-preview');
+		return;
+	}
+
+	if (is_array($value)) {
+		echo $field['type'];
+		echo '<pre>';
+		print_r($value);
+		echo '</pre>';
+ 		return;
+	}
+
+	echo $value;
+}
+
+/**
+ * @function _cortex_buff
+ * @since 0.1.0
+ * @hidden
+ */
+function _cortex_buff($function) {
+	ob_start();
+	$function();
+	$contents = ob_get_contents();
+	ob_end_clean();
+	return $contents;
+}
+
+/**
+ * @function _cortex_file_empty
+ * @since 0.1.0
+ * @hidden
+ */
 function _cortex_file_empty($file) {
 	clearstatcache();
 	return filesize($file) == 0;
